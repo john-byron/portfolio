272255a78f91838a2118240ad2d17bcd4658e125
john-byron <68835067+john-byron@users.noreply.github.com>
2020-12-14 14:37:27 -0500

    Added Image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 id="header">John Byron</h1>
+      <img id="head-image" src="https://image.flaticon.com/icons/png/512/2828/2828957.png" alt="Man working on laptop" width="250px"  />
     </div>
   );
 }
