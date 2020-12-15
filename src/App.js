ed37385c3aeb4ab22bede19ff9d8c4ffdfedfba9
john-byron <68835067+john-byron@users.noreply.github.com>
2020-12-14 21:28:03 -0500

    Experimenting with paragraph settings

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 id="header">John Byron</h1>
+      <h1 className="title" id="header">John Byron</h1>
+
       <img id="head-image" src="https://image.flaticon.com/icons/png/512/2828/2828957.png" alt="Man working on laptop" width="250px"  />
+
+      <h2 className="title main-text" id="about-title">About Me</h2>
+
+      <p className="section main-text" id="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque tempore incidunt mollitia dignissimos odit earum voluptas quibusdam maxime id voluptatum, cupiditate maiores, harum reiciendis architecto saepe voluptate minus officiis?</p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam rerum quae eius consequatur, temporibus tempora velit cumque repellendus magni nesciunt suscipit beatae cum corrupti voluptate quisquam consectetur corporis excepturi. Itaque.</p>
+
+
     </div>
   );
 }
