9342744291effb8635e3dddd4a707a10c40e358e
john-byron <68835067+john-byron@users.noreply.github.com>
2020-12-18 10:50:08 -0500

    Testing react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1 className="title" id="title">Background Missionaries</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <h2 className="title" id="subtitle">Keeping Missionaries on the Feild</h2>
-
-      <img id="head-image" src="https://image.flaticon.com/icons/png/512/2828/2828957.png" alt="Man working on laptop" width="250px"  />
-
-      <h3 className="title main-text" id="about-title">What We Do</h3>
-
-      <p className="section main-text" id="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque tempore incidunt mollitia dignissimos odit earum voluptas quibusdam maxime id voluptatum, cupiditate maiores, harum reiciendis architecto saepe voluptate minus officiis?</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam rerum quae eius consequatur, temporibus tempora velit cumque repellendus magni nesciunt suscipit beatae cum corrupti voluptate quisquam consectetur corporis excepturi. Itaque.</p>
-
-
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Link 
+} from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title" id="title">Background Ministries</h1>
+
+      <h2 className="title" id="subtitle">Putting and Keeping Missionaries in the Field</h2>
+
+<div id="image-container">
+      <div className="container" id="laptop">
+        <h2>We Do This</h2>
+      <img id="head-image" src="https://image.flaticon.com/icons/png/512/2828/2828957.png" alt="Man working on laptop" width="250px"  /></div>
+      
+      <div className="container" id="serve">
+      <h2>While You Do This</h2>
+      <img id="soup-image" src="https://media.istockphoto.com/vectors/volunteers-feed-the-poor-beggar-and-homeless-people-vector-id1188168141?k=6&m=1188168141&s=170667a&w=0&h=LnrJ7YQkau5nZZAWHyX737MuaxH6yjKoMQ7CxxEV4Tk=" alt="People serving soup" width="250px"  />
+      </div>
+      </div>
+      <h3 className="title main-text" id="about-title">What We Do</h3>
+
+      <p className="section main-text" id="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque tempore incidunt mollitia dignissimos odit earum voluptas quibusdam maxime id voluptatum, cupiditate maiores, harum reiciendis architecto saepe voluptate minus officiis?</p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam rerum quae eius consequatur, temporibus tempora velit cumque repellendus magni nesciunt suscipit beatae cum corrupti voluptate quisquam consectetur corporis excepturi. Itaque.</p>
+
+
+    </div>
+  );
+}
+
+export default App;
