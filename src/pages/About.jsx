9c51d0e67dec1d55f4e16c093487557154813409
john-byron <68835067+john-byron@users.noreply.github.com>
2021-04-03 00:42:27 -0400

    updated quote on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,10 @@ function About() {
 
             <img src="https://static.wixstatic.com/media/9156348f9b554babaa4d6c009ce2db0f.jpg/v1/fit/w_924,h_520/9156348f9b554babaa4d6c009ce2db0f.jpg" alt="Wheat blowing in the wind"/>
 
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum accusamus cumque beatae adipisci, quibusdam aspernatur assumenda quaerat itaque id deserunt molestias ipsum nostrum laudantium perspiciatis sed ipsa, blanditiis quae nemo.</p>
+            <p className="Bible-Quote main-text" id="Matt9">And Jesus went throughout all the cities and villages, teaching in their synagogues and proclaiming the gospel of the kingdom and healing every disease and every affliction. When he saw the crowds, he had compassion for them, because they were harassed and helpless, like sheep without a shepherd. Then he said to his disciples, “The harvest is plentiful, but the laborers are few; therefore pray earnestly to the Lord of the harvest to send out laborers into his harvest.”
+​</p>
+
+<div className="Quote-Name"><em>~ Matthew 9. 35 - 38</em></div>
         </div>
     )
 }
