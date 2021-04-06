b021a8065cc1b130b072990f8c4fa767051ee592
john-byron <68835067+john-byron@users.noreply.github.com>
2021-04-06 11:24:14 -0400

    Adjusted the Statement of Faith text

--- src/pages/StatementOfFaith.jsx
+++ src/pages/StatementOfFaith.jsx
@@ -4,29 +4,108 @@ import React from 'react';
 function StatementOfFaith() {
     return (
         <div className="faith">
-    <div>
-        <h1 className="title" id="statementOfFaith-title">LetsFundThee's Statement of Faith</h1>
-    </div>
-    <div className="faith" id="statementOfFaith-rules">
-    <ol >
-        <li><b>The Trinity: </b>There is 1 single God consisting of 3 separate but equal parts. They consist of: God the Father; God the Son; and God the Holy Spirit. </li>
-        <li><b>God the Father: </b></li>
-        <li><b>God the Son: </b></li>
-        <li><b>God the Holy Spirit: </b></li>
-        <li><b>The Bible: </b></li>
-        <li><b>The Church: </b></li>
-        <li><b>The Mission of the Church: </b></li>
-        <li><b>Evangelism: </b>To take the Good News of Jesus Christ to the people who do not know of the Gospel. To show the love of God through both our words that we say and the actions that we take.</li>
-        <li><b>The Fall of Man: </b>That God has created man in His own image and that we were in perfect fellowship with God. However, man has chosen to sin against God, ruining the perfect fellowship. Now, we are subject to God’s eternal wrath and are in need of salvation from His wrath.</li>
-        <li><b>Grace: </b>Jesus Christ has died for all of our past, present, and future sins. Although, we are covered by this grace, we try not to continue in our sin.</li>
-        <li><b>Salvation: </b>God, being rich in mercy and kindness, has provided a way that we may restore our broken relationship with Him through His son Jesus Christ, who died on a cross for our sins. God has made this the only way to restore the relationship with Him and made it so no one could be justified by their works. Jesus died so that anyone in the world may be saved and is able to have fellowship with the Father freely and without shame of their sin.</li>
-        <li><b>Baptism: </b>Baptism is not mandatory for salvation but is an outward showing of obedience. It is a statement to the world that they are a follower of Jesus Christ, and if they are not willing to take this step of obedience, their faith in Jesus should be in question.</li>
-        <li><b>The End of Days: </b></li>
-        <li><b>Stewardship: </b>We try to take what God has given us and manage it wisely, to make sure what the donors have given is going to the cause that they are donating to, and to help the missionaries that are going through the website be good stewards themselves.</li>
-        <li><b>We are One: </b>We may come from different denominations, speak different languages, and/or have different missions, but we are all followers of Jesus, we are all a part of the body, serving those that are in need of Christ, and we are all a part of the universal church.</li>
-    </ol>
-    </div>
-</div>
+            <div id="statementOfFaith-header">
+                <h1 className="title" id="statementOfFaith-title">LetsFundThee's Statement of Faith</h1>
+
+                <p className="faith" id="statementOfFaith-intro">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui provident, blanditiis ipsa accusamus a laudantium corporis ut voluptates, eos praesentium quo placeat quae distinctio deserunt tempora quidem alias hic molestiae.
+        </p>
+            </div>
+            <div className="faith" id="statementOfFaith-rules">
+                <ol >
+                    <li><b>The Trinity: </b>
+                        <span className="statement-text">
+                            There is 1 single God consisting of 3 separate but equal parts. They consist of: God the Father; God the Son; and God the Holy Spirit.
+                    </span>
+                    </li>
+
+                    <li><b>God the Father: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>God the Son: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>God the Holy Spirit: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>The Bible: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>The Church: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>The Mission of the Church: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>Evangelism: </b>
+                        <span className="statement-text">
+                            To take the Good News of Jesus Christ to the people who do not know of the Gospel. To show the love of God through both our words that we say and the actions that we take.
+                        </span>
+                    </li>
+
+                    <li><b>The Fall of Man: </b>
+                        <span className="statement-text">
+                            That God has created man in His own image and that we were in perfect fellowship with God. However, man has chosen to sin against God, ruining the perfect fellowship. Now, we are subject to God’s eternal wrath and are in need of salvation from His wrath.
+                        </span>
+                    </li>
+
+                    <li><b>Grace: </b>
+                        <span className="statement-text">
+                            Jesus Christ has died for all of our past, present, and future sins. Although, we are covered by this grace, we try not to continue in our sin.
+                        </span>
+                    </li>
+
+                    <li><b>Salvation: </b>
+                        <span className="statement-text">
+                            God, being rich in mercy and kindness, has provided a way that we may restore our broken relationship with Him through His son Jesus Christ, who died on a cross for our sins. God has made this the only way to restore the relationship with Him and made it so no one could be justified by their works. Jesus died so that anyone in the world may be saved and is able to have fellowship with the Father freely and without shame of their sin.
+                        </span>
+                    </li>
+
+                    <li><b>Baptism: </b>
+                        <span className="statement-text">
+                            Baptism is not mandatory for salvation but is an outward showing of obedience. It is a statement to the world that they are a follower of Jesus Christ, and if they are not willing to take this step of obedience, their faith in Jesus should be in question.
+                        </span>
+                    </li>
+
+                    <li><b>The End of Days: </b>
+                        <span className="statement-text">
+
+                        </span>
+                    </li>
+
+                    <li><b>Stewardship: </b>
+                        <span className="statement-text">
+                            We try to take what God has given us and manage it wisely, to make sure what the donors have given is going to the cause that they are donating to, and to help the missionaries that are going through the website be good stewards themselves.
+                        </span>
+                    </li>
+
+                    <li><b>We are One: </b>
+                        <span className="statement-text">
+                            We may come from different denominations, speak different languages, and/or have different missions, but we are all followers of Jesus, we are all a part of the body, serving those that are in need of Christ, and we are all a part of the universal church.
+                        </span>
+                    </li>
+
+                </ol>
+            </div>
+        </div>
     )
 }
 
