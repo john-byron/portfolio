ca7bd352ea476239d2da13e6d2a527b89a634a5c
john-byron <68835067+john-byron@users.noreply.github.com>
2021-04-11 00:22:20 -0400

    Created a page about interesting facts

---
+++ src/pages/InterestingFacts.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function InterestingFacts() {
+    return (
+        <div>
+            {/* This page will contain facts that a missionary might find interesting. This information could be used for new or current missionaries, but is mainly targeted towards the new missionaries that are trying to get into the field. Not sure yet how detailed the page will become and if there would be sub-pages needed to cover some topics in more depth.*/}
+
+            {/* Note: This information will be sourced by articles that I have found on the internet and my own notes that I have gathered while researching*/}
+
+            {/* Reminder: Make sure to add information about Voluntourism */}
+
+
+            <h1 className="title" id="facts-header">
+                Some Interesting Facts About Being A Missionary
+            </h1>
+
+            <p className="main-text" id="facts-opener">
+                This page is dedicated for information that we think that new and current missionaries might would like to know. All information will be sourced to their original sources that they were found on for your reference if you would like to visit the sources. This website is not in any way connected, a part of, or affiliated with these references that are listed and do not necessarily agree or align with these resources outside of the points that are referenced on this site. 
+            </p>
+
+            
+        </div>
+    )
+}
+
+export default InterestingFacts
