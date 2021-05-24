3b229297c0e93c093bfa8ea91ab86c140b021379
john-byron <68835067+john-byron@users.noreply.github.com>
2021-05-23 23:05:34 -0400

    Added a READ ME to the map section

--- src/pages/Maps/BasicMap.jsx
+++ src/pages/Maps/BasicMap.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+// import {d3, select, json} from 'https://d3js.org/d3.v6.min.js';
+
 
 function BasicMap() {
     return (
+
+        
         <div>
+            json('https://d3js.org/us-10m.v1.json')
+
+
            <h1 className="map-title" id="basicMap-title">Basic Map</h1> 
 
-          <svg width='960' height='500' className="map" id="basicMap"></svg>  
+          <svg width='960' height='500' className="map" id="basicMap">
+              
+              </svg>  
 
         </div>
     )
---
+++ src/pages/Maps/READ_ME_MAPS.txt
@@ -0,0 +1,10 @@
+This area will be for several different types of maps. Below is the list of the different maps and descriptions about the maps. (All these maps might be combined into one map)
+
+- Basic map: 
+
+- World Watch List map: Info from Open Door's "World Watch List"
+
+- 10/40 Box map: Map highlighting the 10/40 Box (The area where Christianity is suppressed and followers are persecuted the most)
+
+- Prosperity Church map: Showing where the Prosperity Church (A church that preaches a false Christianity)
+
