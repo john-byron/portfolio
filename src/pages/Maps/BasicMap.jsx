9a1e09e93333496d9d205d0766ac45a0f825e3e4
john-byron <68835067+john-byron@users.noreply.github.com>
2021-06-25 23:47:53 -0400

    New Branch

--- src/pages/Maps/BasicMap.jsx
+++ src/pages/Maps/BasicMap.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 // import {d3, select, json} from 'https://d3js.org/d3.v6.min.js';
 
 
+// Source for world dataset use the CIA world factbook.
+// There is an API for this information in Git Hub at
+// src = "https://github.com/iancoleman/cia_world_factbook_api"
+
+
 function BasicMap() {
     return (
 
