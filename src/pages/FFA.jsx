591b55e236323bc0d3bf88bc274217df49c4b859
john-byron <68835067+john-byron@users.noreply.github.com>
2021-06-28 22:51:00 -0400

    Added list and dummy text to FFA page

--- src/pages/FFA.jsx
+++ src/pages/FFA.jsx
@@ -5,8 +5,49 @@ function FFA() {
         <div>
             <h1>Fully Funded Academy</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorum ullam illum ab minus dolore impedit aperiam vel, mollitia suscipit atque molestiae provident cupiditate aliquam magnam numquam assumenda aut! Numquam.</p>
-            
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorum ullam illum ab minus dolore impedit aperiam vel, mollitia suscipit atque molestiae provident cupiditate aliquam magnam numquam assumenda aut! Numquam.
+            </p>
+            <ol className="RoadMap" id="FREEDOM">
+
+                <li id="F">Focus Your Vision</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="R">Run Your Research</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="E">Enlist Your Team</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="E">Enhance Your Brand</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="D">Deploy Your Team</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="O">Organize Your Ask</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+                <li id="M">Make Your Difference</li>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
+                </p>
+
+            </ol>
+
+
             <button src="https://go.fullyfundedacademy.com/roadmap">Click Me</button>
         </div>
     )
