57aade21e254a026e67f29a55fb2f64f83edd80b
john-byron <68835067+john-byron@users.noreply.github.com>
2021-06-30 00:11:33 -0400

    Added ffa roadmap

--- src/pages/FFA.jsx
+++ src/pages/FFA.jsx
@@ -2,53 +2,63 @@ import React from 'react'
 
 function FFA() {
     return (
-        <div>
-            <h1>Fully Funded Academy</h1>
+        <div className="ffa">
+            <h1 className="ffa-title">Fully Funded Academy</h1>
 
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorum ullam illum ab minus dolore impedit aperiam vel, mollitia suscipit atque molestiae provident cupiditate aliquam magnam numquam assumenda aut! Numquam.
             </p>
             <ol className="RoadMap" id="FREEDOM">
 
-                <li id="F">Focus Your Vision</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <li className="RoadMap" id="F">Focus Your Vision</li>
 
-                <li id="R">Run Your Research</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <ol className="steps">
+                    <li>Have a written mission statement.</li>
+                    <li>Have a written vision statement.</li>
+                    <li>Have a written timeline.</li>
+                    <li>Have a written budget.</li>
+                </ol>
 
-                <li id="E">Enlist Your Team</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <li className="RoadMap" id="R">Run Your Research</li>
+                <ol className="steps">
+                    <li>Read about 1 - 3 missionaries that have successfully been to the field that you are looking to go to.</li>
+                    <li>Try to analyse and see what were their successes, how did they measure success, how did they accomplish them, what were their pitfalls, and how they overcame those pitfalls.</li>
+                    <li>Have a one on one conversation with at least 3 missionaries about their experiences while they were in the field.</li>
+                    <li>Compare your notes from step 1 with the things that you have read and the conversations that you have had and make changes where needed.</li>
+                </ol>
 
-                <li id="E">Enhance Your Brand</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <li className="RoadMap" id="E">Enlist Your Team</li>
+                <ol className="steps">
+                    <li>Find yourself a CEO to help you with your journey.</li>
+                    <li>Have an elevator speech ready and a 1 page document prepared to hand out briefly describing your mission.</li>
+                    <li>Try and find a few more members, 3 or 4, to join your team to help you with your mission.</li>
+                </ol>
 
-                <li id="D">Deploy Your Team</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <li className="RoadMap" id="E">Enhance Your Brand</li>
+                <ol className="steps">
+                    <li>Find color themes and creat a logo that fits around your mission.</li>
+                    <li>Create a website where people can read updates and possibly give towards your mission.</li>
+                    <li>Have an email and/or newsletter sent out at least every month to keep others informed about your mission.</li>
+                </ol>
 
-                <li id="O">Organize Your Ask</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                <li className="RoadMap" id="D">Deploy Your Team</li>
+                <ol className="steps">
+                    <li>Create a fictional bio of what you think would be the perfect donor for you.</li>
 
-                <li id="M">Make Your Difference</li>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa ratione praesentium aliquam expedita illum obcaecati facere molestias nostrum quaerat modi harum numquam laborum ea similique, beatae repudiandae itaque aperiam.
-                </p>
+                    <li>Start sending out your team to talk to others about your mission, introducing you to other potential donors, and opening up doors that might be closed otherwise.</li>
+                </ol>
 
+                <li className="RoadMap" id="O">A Few Things That You Might Want To Note</li>
+                <ol className="steps">
+                    <li>Be prepared with a customized message (not your elevator speech) when presenting your mission.</li>
+                    <li>Make sure that you prepare your message to the right audience.</li>
+                    <li>Know the exact dollar amount that you need and in what area of the budget that it is when presenting.</li>
+                    <li>Celebrate both the small and large victories with your team and donors.</li>
+                </ol>
             </ol>
 
 
-            <button src="https://go.fullyfundedacademy.com/roadmap">Click Me</button>
+            <button src="https://go.fullyfundedacademy.com/roadmap">Want To Learn More?</button>
         </div>
     )
 }
