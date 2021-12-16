b92c087269ced7c2530f70494742eb0d17098fbd
John Byron <john@byronsolutions.tech>
2021-12-16 00:36:16 -0500

    Added List of Services within Administration page

--- src/pages/AdminHelp.jsx
+++ src/pages/AdminHelp.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 function AdminHelp() {
     return (
         <div>
-            <h1>Let Us Help You With Your Administration Needs</h1>
+            <h1>Let Us Help You With Your Administration Needs</h1>  
+
+            
+                <li className="admin">Create and Manage a Website </li>
+                <li className="admin">Accounting / Bookkeeping</li>
+                <li className="admin">Social Media Management</li>
+                <li className="admin">Make And Coordinate Appointments While Off The Field</li>
+                <li className="admin">Help Produce Weekly Or Monthly Email / Newsletter</li>
+                <li className="admin"></li>
+
+                
+            
+
         </div>
     )
 }
